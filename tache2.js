1124c7f5e2a6a0dfce0583793faa99e768291353
Mouhamed Ndiaye <mouhamethndiaye123@gmail.com>
2024-07-16 11:36:26 +0000

    ajout des fichiers du tache2 de js

---
+++ tache2.js
@@ -0,0 +1,6 @@
+// Écrivez un script qui affiche le message "Hello World !" sur la page. Puis un script qui affiche “hello world” en utilisant prompt et alert
+
+document.write('Hello world');
+
+prompt("Ecrit hello world")
+alert('Hello world')
